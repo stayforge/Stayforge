782999bfc1936e1bc8f65c273a45bf9dbb14e388
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2024-12-02 17:18:31 +0900

    Add PUT endpoint to update branch by ID
    This commit introduces a new PUT endpoint to update an existing branch based on its ID in the `api/branch/view.py` file. It includes error handling for invalid ID formats and returns appropriate responses for different scenarios such as resource not found or update errors. Some cleanup was also done by removing unused imports and renaming the `get_branch` function to `get_branches` to better reflect its purpose.

--- api/branch/view.py
+++ api/branch/view.py
@@ -1,10 +1,7 @@
 import time
 from typing import List
-
-import pydantic_core
 from bson import ObjectId
 from fastapi import *
-from pydantic import ValidationError
 
 from settings import logger
 from .models import *
@@ -20,42 +17,60 @@ class BranchResponses(BaseResponses):
 @router.post("/", response_model=BranchResponses)
 async def create_branch(request: Request, data: BranchInput):
     str_time = time.perf_counter()
-    _id = await branch_repository.insert_one(data.model_dump(), request=request)
-    d = await branch_repository.find_one(query={"_id": ObjectId(_id)})
-    if not d:
+    try:
+        _id = await branch_repository.insert_one(data.model_dump(), request=request)
+        d = await branch_repository.find_one(query={"_id": ObjectId(_id)})
+        if not d:
+            return BranchResponses(
+                status=404,
+                detail="Resource maybe created. But can't found it.",
+                used_time=time.perf_counter() - str_time,
+                data=None
+            )
         return BranchResponses(
-            status=404,
-            detail="Resource maybe created. But can not access it.",
+            data=[Branch.from_mongo(d)],
+            used_time=(time.perf_counter() - str_time) * 1000
+        )
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        return BranchResponses(
+            status=500,
+            detail=str(e),
             used_time=time.perf_counter() - str_time,
             data=None
         )
-    return BranchResponses(
-        data=[Branch.from_mongo(d)],
-        used_time=(time.perf_counter() - str_time) * 1000
-    )
 
 
 @router.get("/", response_model=BranchResponses)
-async def get_branch(
+async def get_branches(
         request: Request,
         name: str = Query(default=None),
         address: str = Query(default=None),
         telephone: str = Query(default=None)
 ):
     str_time = time.perf_counter()
-    query = {key: value for key, value in {
-        "name": name, "address": address, "telephone": telephone
-    }.items() if value}
-    ds = await branch_repository.find_many(query=query, request=request)
+    try:
+        query = {key: value for key, value in {
+            "name": name, "address": address, "telephone": telephone
+        }.items() if value}
+        ds = await branch_repository.find_many(query=query, request=request)
 
-    result = []
-    for d in ds:
-        result.append(Branch.from_mongo(d))
+        result = []
+        for d in ds:
+            result.append(Branch.from_mongo(d))
 
-    return BranchResponses(
-        data=result,
-        used_time=(time.perf_counter() - str_time) * 1000
-    )
+        return BranchResponses(
+            data=result,
+            used_time=(time.perf_counter() - str_time) * 1000
+        )
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        return BranchResponses(
+            status=500,
+            detail="An error occurred while retrieving branches.",
+            used_time=time.perf_counter() - str_time,
+            data=None
+        )
 
 
 @router.get("/<id>", response_model=BranchResponses)
@@ -122,6 +137,49 @@ async def delete_branch(
             data=None,
             used_time=(time.perf_counter() - str_time) * 1000,
             detail=f"Successfully. [{d}] Resource(s) deleted."
+        )
+    except Exception as e:
+        logger.error(e, exc_info=True)
+        return BranchResponses(
+            status=500,
+            detail=str(e),
+            used_time=time.perf_counter() - str_time,
+            data=None
+        )
+
+
+@router.put("/{id}", response_model=BranchResponses, responses={
+    404: {
+        "description": "Resource maybe changed. But can't found it.",
+    }
+})
+async def put_branch(
+        request: Request,
+        id: str,
+        data: BranchInput
+):
+    str_time = time.perf_counter()
+    try:
+        if not ObjectId.is_valid(id):
+            return BranchResponses(
+                status=400,
+                detail="Invalid ID format",
+                used_time=(time.perf_counter() - str_time) * 1000,
+                data=None
+            )
+        await branch_repository.update_one(query={"_id": ObjectId(id)}, update=data.model_dump(),
+                                           request=request)
+        d = await branch_repository.find_one(query={"_id": ObjectId(id)}, request=request)
+        if not d:
+            return BranchResponses(
+                status=404,
+                detail="Resource maybe changed. But can't found it.",
+                used_time=(time.perf_counter() - str_time) * 1000,
+                data=None
+            )
+        return BranchResponses(
+            data=[Branch.from_mongo(d)],
+            used_time=(time.perf_counter() - str_time) * 1000
         )
     except Exception as e:
         logger.error(e, exc_info=True)
