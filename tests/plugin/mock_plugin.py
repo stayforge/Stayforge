85fc46b542f2e9e1af0fdc26551c66fbca6cffa4
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2024-12-19 22:22:04 +0800

    script_path

--- tests/plugin/mock_plugin.py
+++ tests/plugin/mock_plugin.py
@@ -1,16 +1,18 @@
 from flask import Flask, send_from_directory, Blueprint, Response
 import os
 
+script_path = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__)
 bp = Blueprint('demo-plugin', __name__)
 
 @bp.route('/', methods=['GET'])
 def plugin():
-    return send_from_directory('./', 'plugin.yaml')
+    return send_from_directory(script_path, 'plugin.yaml')
 
 @bp.route('/permissions.json', methods=['GET'])
 def permissions():
-    return send_from_directory('./', 'permissions.json')
+    return send_from_directory(script_path, 'permissions.json')
 
 
 app.register_blueprint(bp, url_prefix='/demo-namespace/demo-plugin')
