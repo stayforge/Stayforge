134faf89c98dc86a4f585c89bf89b25e0df3ec6b
Junpei Ikuta <171251543+Iktahana@users.noreply.github.com>
2024-12-25 19:42:23 +0900

    FIXED Refactor "Key" to "Branch" in branch models and views
    Replaced "Key" terminology with "Branch" across models, repositories, and API endpoints for better clarity and alignment with functionality. Updated class names, field descriptions, and method names to reflect the change. This improves code readability and consistency throughout the project.

--- api/branch/models.py
+++ api/branch/models.py
@@ -7,9 +7,9 @@ from repository import MongoRepository
 
 from faker import Faker
 
-collection_name = 'key'
+collection_name = 'branch'
 
-key_repository = MongoRepository(
+branch_repository = MongoRepository(
     database=settings.DATABASE_NAME,
     collection=collection_name,
     client=database.client
@@ -18,29 +18,29 @@ key_repository = MongoRepository(
 faker = Faker('ja_JP')
 
 
-class KeyInput(BaseModel):
+class BranchInput(BaseModel):
     name: str = Field(
         ...,
         examples=[f"ホテルステイフォージ{faker.town()}"],
-        description="The name of the hotel key. By default, it combines a base name with a random town."
+        description="The name of the hotel branch. By default, it combines a base name with a random town."
     )
     postcode: str = Field(
         "000-0000",
         examples=[faker.postcode()],
-        description="The postal code of the key location."
+        description="The postal code of the branch location."
     )
     address: str = Field(
         "000-0000",
         examples=[
             f"{faker.administrative_unit()}{faker.city()}{faker.town()}{faker.chome()}{faker.ban()}{faker.gou()}"
         ],
-        description="The full effective of the key, including administrative unit, city, town, and detailed location."
+        description="The full effective of the branch, including administrative unit, city, town, and detailed location."
     )
     telephone: str = Field(
         examples=[f"{faker.phone_number()}"],
-        description="The contact telephone number for the key."
+        description="The contact telephone number for the branch."
     )
 
 
-class Key(KeyInput, StayForgeModel):
+class Branch(BranchInput, StayForgeModel):
     pass
--- api/branch/view.py
+++ api/branch/view.py
@@ -9,12 +9,12 @@ from ..schemas import BaseResponses
 router = APIRouter()
 
 
-class KeyResponses(BaseResponses):
-    data: Optional[List[Key]]
+class BranchResponses(BaseResponses):
+    data: Optional[List[Branch]]
 
 
-@router.get("/", response_model=KeyResponses)
-async def get_keys(
+@router.get("/", response_model=BranchResponses)
+async def get_branches(
         request: Request,
         name: str = Query(default=None),
         address: str = Query(default=None),
@@ -22,16 +22,16 @@ async def get_keys(
 ):
     str_time = time.perf_counter()
     try:
-        query = {key: value for key, value in {
+        query = {branch: value for branch, value in {
             "name": name, "effective": address, "telephone": telephone
         }.items() if value}
-        ds = await key_repository.find_many(query=query, request=request)
+        ds = await branch_repository.find_many(query=query, request=request)
 
         result = []
         for d in ds:
-            result.append(Key.from_mongo(d))
+            result.append(Branch.from_mongo(d))
 
-        return KeyResponses(
+        return BranchResponses(
             data=result,
             used_time=(time.perf_counter() - str_time) * 1000
         )
@@ -40,54 +40,54 @@ async def get_keys(
         return handle_error(e, str_time)
 
 
-@router.get("/{id}", response_model=KeyResponses)
-async def get_key(
+@router.get("/{id}", response_model=BranchResponses)
+async def get_branch(
         request: Request,
         id: str
 ):
     str_time = time.perf_counter()
     try:
         if not ObjectId.is_valid(id):
-            return KeyResponses(
+            return BranchResponses(
                 status=400,
                 detail="Invalid ID format",
                 used_time=(time.perf_counter() - str_time) * 1000,
                 data=None
             )
-        d = await key_repository.find_one(query={"_id": ObjectId(id)}, request=request)
+        d = await branch_repository.find_one(query={"_id": ObjectId(id)}, request=request)
         if not d:
             return handle_resource_not_found_error(str_time)
 
-        return KeyResponses(
-            data=[Key.from_mongo(d)],
+        return BranchResponses(
+            data=[Branch.from_mongo(d)],
             used_time=(time.perf_counter() - str_time) * 1000
         )
     except Exception as e:
         return handle_error(e, str_time)
 
 
-@router.post("/", response_model=KeyResponses, responses={
+@router.post("/", response_model=BranchResponses, responses={
     409: {
         "description": "Resource maybe created. But can't found it.",
     }
 })
-async def create_key(request: Request, data: KeyInput):
+async def create_branch(request: Request, data: BranchInput):
     str_time = time.perf_counter()
     try:
-        _id = await key_repository.insert_one(data.model_dump(), request=request)
-        d = await key_repository.find_one(query={"_id": ObjectId(_id)})
+        _id = await branch_repository.insert_one(data.model_dump(), request=request)
+        d = await branch_repository.find_one(query={"_id": ObjectId(_id)})
         if not d:
             return handle_after_write_resource_not_found_error(str_time)
-        return KeyResponses(
-            data=[Key.from_mongo(d)],
+        return BranchResponses(
+            data=[Branch.from_mongo(d)],
             used_time=(time.perf_counter() - str_time) * 1000
         )
     except Exception as e:
         return handle_error(e, str_time)
 
 
-@router.delete("/{id}", response_model=KeyResponses)
-async def delete_key(
+@router.delete("/{id}", response_model=BranchResponses)
+async def delete_branch(
         request: Request,
         id: str
 ):
@@ -95,11 +95,11 @@ async def delete_key(
     try:
         if not ObjectId.is_valid(id):
             return handle_invalid_id_format_error(str_time)
-        d = await key_repository.delete_one(query={"_id": ObjectId(id)}, request=request)
+        d = await branch_repository.delete_one(query={"_id": ObjectId(id)}, request=request)
         if not d:
             return handle_resource_not_found_error(str_time)
 
-        return KeyResponses(
+        return BranchResponses(
             data=None,
             used_time=(time.perf_counter() - str_time) * 1000,
             detail=f"Successfully. [{d}] Resource(s) deleted."
@@ -108,27 +108,27 @@ async def delete_key(
         return handle_error(e, str_time)
 
 
-@router.put("/{id}", response_model=KeyResponses, responses={
+@router.put("/{id}", response_model=BranchResponses, responses={
     409: {
         "description": "Resource maybe changed. But can't found it.",
     }
 })
-async def put_key(
+async def put_branch(
         request: Request,
         id: str,
-        data: KeyInput
+        data: BranchInput
 ):
     str_time = time.perf_counter()
     try:
         if not ObjectId.is_valid(id):
             return handle_invalid_id_format_error(str_time)
-        await key_repository.update_one(query={"_id": ObjectId(id)}, update=data.model_dump(),
-                                           request=request)
-        d = await key_repository.find_one(query={"_id": ObjectId(id)}, request=request)
+        await branch_repository.update_one(query={"_id": ObjectId(id)}, update=data.model_dump(),
+                                        request=request)
+        d = await branch_repository.find_one(query={"_id": ObjectId(id)}, request=request)
         if not d:
             return handle_after_write_resource_not_found_error(str_time)
-        return KeyResponses(
-            data=[Key.from_mongo(d)],
+        return BranchResponses(
+            data=[Branch.from_mongo(d)],
             used_time=(time.perf_counter() - str_time) * 1000
         )
     except Exception as e:
